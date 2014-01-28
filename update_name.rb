e09490fc1a22feaef0f128b061b3babd0cb895f4
sh4869 <nobuk4869@gmail.com>
2014-01-28 18:11:48 +0900

    Bug fixed

--- update_name.rb
+++ update_name.rb
@@ -7,56 +7,61 @@ CONSUMER_SECRET = YOUR_CONSUMER_SECRET
 ACCESS_TOKEN    = YOUR_ACCESS_TOKEN
 ACCESS_SECRET   = YOUR_ACCESS_SECRET
 
+
 @rest_client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = CONSUMER_KEY
-    config.consumer_secret     = CONSUMER_SECRET
-    config.access_token        = ACCESS_TOKEN
-    config.access_token_secret = ACCESS_SECRET
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_SECRET
 end
 
 @stream_client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key       = CONSUMER_KEY
-    config.consumer_secret    = CONSUMER_SECRET
-    config.oauth_token        = ACCESS_TOKEN
-    config.oauth_token_secret = ACCESS_SECRET
+  config.consumer_key       = CONSUMER_KEY
+  config.consumer_secret    = CONSUMER_SECRET
+  config.oauth_token        = ACCESS_TOKEN
+  config.oauth_token_secret = ACCESS_SECRET
 end
 
 @orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
-@regexp = /^(?:RT )?@#{@screen_name} *update_name( (.+))?/
+@regexp = /(.+)?\(@sh4869sh\)(.+)?/
 @count = 1
 @time = Time.now
 @day = @time.strftime("%x %H:%M")
 
 def update_name(status)
-    begin
-        name = status.text.match(@regexp)[2]
-        if name && 20 < name.length
-            text = "長すぎます(#{count}回目)"
-            raise "New name is too long"
-        end
+  begin
+    name = status.text.gsub(/\(@sh4869sh\)/,"")
+    puts "#{status.user.screen_name} #{name}"
+    if name && 20 < name.length
+      text = "長すぎます(#{count}回目)"
+    raise "New name is too long"
+  end
 
-        @rest_client.update_profile(name: name)
-        text = @orig_name == name ? "元に戻しました" : "#{name} に改名しました!"
-    rescue => e
+  @rest_client.update_profile(name: name)
+  text = @orig_name == name ? "元に戻しました" : "#{name} に改名しました!"
+      
+  rescue => e
         p status, status.text
         p e
-    ensure
-        @rest_client.update("@#{status.user.screen_name} #{text}")
-    end
-     file = File.open("un.txt", 'a')
-     file.write (name +" @#{status.user.screen_name} " + @day  + "\n\n")
-     file.close
+      ensure
+    @rest_client.update("@#{status.user.screen_name} #{text}", :in_reply_to_status_id => status.id)
+  end
+  
+  file = File.open("un.txt", 'a')
+  file.write (name +" @#{status.user.screen_name} " + @day  + "\n\n")
+  file.close
 
 end
 
 @rest_client.update("update_name再開しました。(" + @day +")")
 
 @stream_client.user do |object|
-    next unless object.is_a? Twitter::Tweet and object.text.match(@regexp)
+  next unless object.is_a? Twitter::Tweet and object.text.match(@regexp)
 
-    unless object.text.start_with? "RT"
-        update_name(object)
-    else
-        puts "RTです"
-    end
+  unless object.text.start_with? "RT"
+    update_name(object)
+  else
+    puts "RTです"
+  end
 end
+
