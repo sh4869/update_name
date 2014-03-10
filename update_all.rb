521ecb2b8c96b21e2c8e33e3c5b0b4b20fd2cbad
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-03-10 13:21:44 +0900

    (@~)の形の改名にも対応しました。

--- update_all.rb
+++ update_all.rb
@@ -4,7 +4,7 @@ require 'twitter'
 require './keys.rb'
 
 
-rest_client = Twitter::REST::Client.new do |config|
+@rest_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = CONSUMER_KEY
   config.consumer_secret     = CONSUMER_SECRET
   config.access_token        = ACCESS_TOKEN
@@ -20,6 +20,7 @@ end
 
 @orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
 @regexp_name = /^@#{@screen_name}\s+update_name\s+(.+)$/
+@regexp_name_2 = /(.+)?\(@#{@screen_name}\)(.+)?/
 @regexp_url = /^@#{@screen_name}\s+update_url\s+(.+)$/
 @regexp_location = /^@#{@screen_name}\s+update_location\s+(.+)$/
 @count = 1
@@ -28,10 +29,8 @@ end
 
 def update_all(status)
   begin
-    if status.text.match(@regexp_name)
-      
+    if status.text.match(@regexp_name) 
       name = $1
-
       if name && 20 < name.length
         text = "長すぎます(#{@count}回目)"
         raise "New name is too long"
@@ -39,16 +38,27 @@ def update_all(status)
       elsif 1 > name.length
         name = "4869"
         @rest_client.update_profile(name: name)
-        text = "#{name}に改名しました。"
+        text = "#{name}に戻しました。"
       else
         @rest_client.update_profile(name: name)
         text = "#{name}に改名しました。"
-      end 
-
+      end
+    elsif status.text.match(@regexp_name_2)  
+      name = status.text.gsub(/\(@#{@screen_name}\)/,"")      
+      if name && 20 < name.length
+        text = "長過ぎます(#{@count}回目)"
+        raise "New name is too long"
+  @count = @count + 1
+      elsif 1 > name.length
+  name = "4869"
+  @rest_client.update_profile(name: name)
+  text = "#{name}に戻しました。"
+      else 
+  @rest_client.update_profile(name: name)
+  text = "#{name}に改名しました"
+      end
     elsif status.text.match(@regexp_url)
-      
       url = $1
-
       if url && 100  < url.length
         text = "長すぎます(#{@count}回目)"
         raise "New URL is too long"
@@ -61,11 +71,8 @@ def update_all(status)
         @rest_client.update_profile(url: url) 
         text = "urlを#{url} に変更しました"
       end
-
     elsif status.text.match(@regexp_location)
-      
       location = $1
-      
       if location && 30 < location.length
         text = "長すぎます(#{@count}回目)"
         raise "New location is too long"
@@ -78,11 +85,8 @@ def update_all(status)
         @rest_client.update_profile(location: location)
         text = "私は #{location} にいます。" 
       end
-
     else
-      
       return
-
     end
   
     puts "#{status.user.screen_name} #{text}"
@@ -98,7 +102,7 @@ end
 @rest_client.update("update_all再開しました。(" + @day +")")
 
 @stream_client.user do |object|
-  next unless object.is_a? Twitter::Tweet and object.text.match(/(#{@regexp_name}|#{@regexp_url}|#{@regexp_location})/) 
+  next unless object.is_a? Twitter::Tweet and object.text.match(/(#{@regexp_name}|#{@regexp_name_2}|#{@regexp_url}|#{@regexp_location})/) 
 
   unless object.text.start_with? "RT"
     update_all(object)
@@ -106,3 +110,4 @@ end
     puts "RTです"
   end
 end
+
