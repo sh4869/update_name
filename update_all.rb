e720bf2c5d2aadd258ae75b5285822261d0ab509
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-03-15 00:53:20 +0900

    add comment in update_all.rb

--- update_all.rb
+++ update_all.rb
@@ -1,5 +1,4 @@
 # Coding: UTF-8
-
 require 'twitter'
 require './keys.rb'
 
@@ -18,8 +17,8 @@ end
 end
 
 @orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
-@regexp_name = /^@#{@screen_name}\s+update_name\s+(.+)$/
-@regexp_name_2 = /(.+)?\(@#{@screen_name}\)(.+)?/
+@regexp_name = /^@#{@screen_name}\s+update_name\s+(.+)$/ 
+@regexp_name_2 = /(.+)?\(@#{@screen_name}\)(.+)?/ 
 @regexp_url = /^@#{@screen_name}\s+update_url\s+(.+)$/
 @regexp_location = /^@#{@screen_name}\s+update_location\s+(.+)$/
 @count = 1
@@ -28,48 +27,47 @@ end
 
 def update_all(status)
   begin
-    if status.text.match(/(#{@regexp_name}|#{@regexp_name_2})/) 
+    if status.text.match(/(#{@regexp_name}|#{@regexp_name_2})/) #名前の変更
       if status.text.match(@regexp_name)
-        name = $1
+        name = $1  
       elsif status.text.match(@regexp_name_2)  
         name = status.text.gsub(/\(@#{@screen_name}\)/,"")      
       end
       
-      if name && 20 < name.length
+      if name && 20 < name.length  #名前が20文字以上の場合
         text = "長過ぎます(#{@count}回目)"
         raise "New name is too long"
         @count = @count + 1
-      else
-        if 1 > name.length
+      else   
+        if 1 > name.length  #名前がない(0文字)場合
           name = "4869"
         end
        	@rest_client.update_profile(name: name)
         text = "#{name}に改名しました"
       end
     
-    elsif status.text.match(@regexp_url)
-    
-      url = $1
-      if url && 100  < url.length
+    elsif status.text.match(@regexp_url) #URLの変更 
+      url = $1   #抽出
+      if url && 100  < url.length  #URLが100文字以上の場合  
         text = "長すぎます(#{@count}回目)"
         raise "New URL is too long"
         @count = @count  + 1
-      else
-        if url.length  < 1
+      else 
+        if url.length  < 1  #URLがない(0文字)の場合
           url = "http://sh4869.net"
 	end
         @rest_client.update_profile(url: url) 
         text = "urlを#{url} に変更しました"
       end
 
-    elsif status.text.match(@regexp_location)
-      location = $1
-      if location && 30 < location.length
+    elsif status.text.match(@regexp_location) #プロフィールの場所の変更
+      location = $1   #抽出
+      if location && 30 < location.length  #場所が30文字以上の場合
         text = "長すぎます(#{@count}回目)"
         raise "New location is too long"
         @count = @count + 1
-      else
-        if location.length < 1
+      else  
+        if location.length < 1 #場所がない(0文字)の場合
           location = "Tokyo"
 	end
 	@rest_client.update_profile(location: location)
@@ -82,12 +80,14 @@ def update_all(status)
   
     puts "#{status.user.screen_name} #{text}"
 
-    rescue => e
+    rescue => e  #エラー処理(エラーをeとして表示させる
       p status, status.text
       p e
     ensure
       @rest_client.update("@#{status.user.screen_name} #{text}", :in_reply_to_status_id => status.id)
+      #変更した相手にツイート
   end
+  #ファイルに書きこんで記録します
   file = File.open("un.txt", "a")
   file.write (name +" @#{status.user.screen_name} " + @day  + "\n\n")
   file.close  
@@ -98,9 +98,9 @@ end
 @stream_client.user do |object|
   next unless object.is_a? Twitter::Tweet and object.text.match(/(#{@regexp_name}|#{@regexp_name_2}|#{@regexp_url}|#{@regexp_location})/) 
 
-  unless object.text.start_with? "RT"
+  unless object.text.start_with? "RT" #当てはまったツイートがRTから始まっていなかった場合
     update_all(object)
-  else
+  else  #始まっていたら
     puts "RTです"
   end
 end
