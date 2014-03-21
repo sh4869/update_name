0723fb5d747d0b0f9cb17124a23ed1d72cf77404
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-03-21 23:40:27 +0900

    use case and update update_all

--- update_all.rb
+++ update_all.rb
@@ -4,12 +4,13 @@ require 'oauth'
 require 'oauth/consumer'
 require './keys.rb'
 
+
 SourcePath = File.expand_path('../', __FILE__)
 TokenFile = "#{SourcePath}/token"
 
 def oauth_first
   @consumer = OAuth::Consumer.new(CONSUMER_KEY ,CONSUMER_SECRET,{
-    :site=>"https://api.twitter.com"
+	:site=>"https://api.twitter.com"
   })
 
   @request_token = @consumer.get_request_token
@@ -51,81 +52,82 @@ end
   config.access_token_secret = ACCESS_SECRET
 end
 
+
 @orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
 @regexp_name = /^@#{@screen_name}\s+update_name\s+(.+)$/ 
 @regexp_name_2 = /(.+)?\(@#{@screen_name}\)(.+)?/ 
 @regexp_url = /^@#{@screen_name}\s+update_url\s+(.+)$/
 @regexp_location = /^@#{@screen_name}\s+update_location\s+(.+)$/
-@count = 1  #連投によるエラーを避けるためにあります。
 @time = Time.now
 @day = @time.strftime("%x %H:%M")
 
 
 def update_all(status)
   begin
-    if status.text.match(/(#{@regexp_name}|#{@regexp_name_2})/) #名前の変更
-      if status.text.match(@regexp_name)
-        name = $1  
-      elsif status.text.match(@regexp_name_2)  
-        name = status.text.gsub(/\(@#{@screen_name}\)/,"")      
-      end
-      
-      if name && 20 < name.length  #名前が20文字以上の場合
-        text = "長過ぎます(#{@count}回目)"
-        raise "New name is too long"
-        @count = @count + 1
-      else   
-        if name.length < 1  #名前がない(0文字)場合
-          name = "4869"
-        end
-       	@rest_client.update_profile(name: name)
-        text = "#{name}に改名しました"
-      end
-    
-    elsif status.text.match(@regexp_url) #URLの変更 
-      url = $1   #抽出
-      if url && 100  < url.length  #URLが100文字以上の場合  
-        text = "長すぎます(#{@count}回目)"
-        raise "New URL is too long"
-        @count = @count  + 1
-      else 
-        if url.length  < 1  #URLがない(0文字)の場合
-          url = "http://sh4869.net"
-	end
-        @rest_client.update_profile(url: url) 
-        text = "urlを#{url} に変更しました"
-      end
-
-    elsif status.text.match(@regexp_location) #プロフィールの場所の変更
-      location = $1   #抽出
-      if location && 30 < location.length  #場所が30文字以上の場合
-        text = "長すぎます(#{@count}回目)"
-        raise "New location is too long"
-        @count = @count + 1
-      else  
-        if location.length < 1 #場所がない(0文字)の場合
-          location = "Tokyo"
-	end
-	@rest_client.update_profile(location: location)
-        text = "私は #{location} にいます。" 
-      end
-    
+	if status.text.match(@regexp_name)
+	  name = $1  
+	  hash = "name"
+	elsif status.text.match(@regexp_name_2)  
+	  name = status.text.gsub(/\(@#{@screen_name}\)/,"")     
+	  hash = "name" 
+	elsif status.text.match(@regexp_url)
+	  url = $1
+	  hash = "url"
+	elsif status.text.match(@regexp_location)
+      location = $1
+	  hash = "location"
     else
-      return
-    end
-  
-    puts "#{status.user.screen_name} #{text}"
+	  return
+	end
+	
+	case hash
+	when "name"
+	  if 1 >  name.length || 20 < name.length  #名前が20文字以上の場合
+		text = "Error:New name is too short or too long.(#{@day})"
+		name = "4869"
+	  else 
+		text = "#{name}に改名しました。"
+	  end
+	when "url"
+	  if 1 > url.length || 100  < url.length  #URLが100文字以上の場合  
+		text = "Error:New URL is too short or too long.(#{@day})"
+		url = "http://sh4869.net"
+	  else
+		text = "urlを#{url} に変更しました"
+	  end
+	when "location"
+	  if 1 > location.length || 30 < location.length  #場所が30文字以上の場合
+		text = "Error:New location is too short or too long.(#{@day})"	
+		location = "Tokyo"
+	  else
+		text = "私は #{location} にいます。" 
+	  end
+	else 
+	  puts "そんなhash存在しません。"
+	end
 
-    rescue => e  #エラー処理(エラーをeとして表示させる
-      p status, status.text
-      p e
-    ensure
-      @rest_client.update("@#{status.user.screen_name} #{text}", :in_reply_to_status_id => status.id)
-      #変更した相手にツイート
+	case hash
+	when "name"
+	  @rest_client.update_profile(name: name)
+	when "url"
+	  @rest_client.update_profile(url: url)
+	when "location"
+	  @rest_client.update_profile(location: location)
+	end
+
+	puts "#{status.user.screen_name} #{text}"
+
+	@rest_client.update("@#{status.user.screen_name} #{text}", :in_reply_to_status_id => status.id)
+
+  rescue Twitter::Error::RequestTimeout
+	sleep(10)
+	retry
+  ensure
+	puts "update you!"
   end
   #ファイルに書きこんで記録します
   file = File.open("un.txt", "a")
-  file.write (name +" @#{status.user.screen_name} " + @day  + "\n\n")
+  file.write ("@#{status.user.screen_name}" + @day  + "\n\n")
   file.close  
 end
 
@@ -135,9 +137,9 @@ end
   next unless object.is_a? Twitter::Tweet and object.text.match(/(#{@regexp_name}|#{@regexp_name_2}|#{@regexp_url}|#{@regexp_location})/) 
 
   unless object.text.start_with? "RT" #当てはまったツイートがRTから始まっていなかった場合
-    update_all(object)
+	update_all(object)
   else  #始まっていたら
-    puts "RTです"
+	puts "RTです"
   end
 end
 
